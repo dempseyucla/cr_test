acd8afa082da0081c9e133dc3a4ac1d8e1629b81
dempseyucla <88111590+dempseyucla@users.noreply.github.com>
2025-03-08 22:09:00 -0800

    Update cr_test.c

--- cr_test.c
+++ cr_test.c
@@ -5,6 +5,8 @@ int one();
 int main() {
 
 puts("hello updated on github.com and then updated on repoleaf");
+  puts("hello updated on github.com and then updated on repoleaf");
+  puts("hello updated on github.com and then updated on repoleaf");
 one();
 
 }
